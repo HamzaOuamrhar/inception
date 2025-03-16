8a4d59a9c4078a167fd81f4a3f60a8c4f1a0c6fe
Hamza Ouamrhar <houamrha@e2r11p3.1337.ma>
2025-03-16 02:12:46 +0000

    hide confidential information inside nginx.conf file

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,3 +1,5 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MO/L=KH/O=1337/OU=student/CN=localhost"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MO/L=KH/O=1337/OU=student/CN=$DOMAIN"
+
+envsubst < nginx.conf > /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
