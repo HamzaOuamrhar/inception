91979dfbb2f4c3bccc7686d68efb635ed8908276
Hamza Ouamrhar <houamrha@e2r2p3.1337.ma>
2025-03-17 02:37:18 +0000

    42

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,7 +3,7 @@ service mariadb start
 mysqladmin -u root password $ROOT_PASSWORD
 
 mysql -u root -p$ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;"
-mysql -u root -p$ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$PASSWORD';"
+mysql -u root -p$ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$USER' IDENTIFIED BY '$PASSWORD';"
 mysql -u root -p$ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$USER'@'%';"
 mysql -u root -p$ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
